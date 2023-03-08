fca80091e1c1c18570960d2fd03d34af505b20bd
mineninja77777 <116883276+mineninja77777@users.noreply.github.com>
2023-03-08 20:12:22 +0000

    Update pxt.json, elements/dust.ts

---
+++ elements/dust.ts
@@ -0,0 +1,18 @@
+let dust = new Element("Dust", {}, img`
+    . . . . . . d . . . . . . . . .
+    . . . . . . . . . . d . . . d .
+    . . . . . . . . . . . . . . . .
+    . . d d . . d . d . . . d . . .
+    . . d . . . . . . . . d d d . .
+    . . . . . . . . . d d d d d . .
+    . . . . . d . . d d d d . . . .
+    . . . . . . . d d d d d d . . .
+    . d d d . . d d d . . . . . . .
+    . d d d d . d d . . . . . . d .
+    . . d d d d d . . . . . . . . .
+    . . . d d d . . . . d d d . . .
+    . . . d d d . . . d d d d d d d
+    . . . d d d d d d d d d d d d d
+    . d d d d d d d d d d d d d d d
+    d d d d d d d d d d d d d d d d
+`, 1)

4d4904f58674b683cd8c2590d2ea412173d892f3
Nif <73426353+Unexian@users.noreply.github.com>
2023-03-08 20:14:37 +0000

    Update pxt.json, main.ts, element.ts

---
+++ element.ts
@@ -0,0 +1,24 @@
+class Element {
+    name: String
+    combos: { [key: string]: string }
+    sprite: Image
+    scale: number
+
+    constructor(name: String, combos: { [key: string]: string } = {}, sprite: Image, scale: number) {
+        this.name = name
+        this.combos = combos
+        this.sprite = sprite
+    }
+
+    toSprite(): Sprite {
+        let sprite = sprites.create(this.sprite, SpriteKind.Element)
+        sprite.data = this
+        sprite.setScale(1 / this.scale, ScaleAnchor.Middle)
+        return sprite
+    }
+
+    merge(other: Sprite) {
+        let otherElement = other.data.name
+        let elementTo = this.combos[otherElement]
+    }
+}
--- main.ts
+++ main.ts
@@ -2,5 +2,4 @@ namespace SpriteKind {
     export const Element = SpriteKind.create()
 }
 
-let elements: {[key: string]: Element} = {}
 let gameMngr = new GameManager()

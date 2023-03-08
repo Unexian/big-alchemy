8c54e45f27bca1161684fc1db1f97129d2e73bf1
Nif <73426353+Unexian@users.noreply.github.com>
2023-03-08 19:12:27 +0000

    Update elements/water.ts, classes/element.ts

--- classes/element.ts
+++ classes/element.ts
@@ -2,8 +2,9 @@ class Element {
     name: String
     combos: {[key: string]: string}
     sprite: Image
+    scale: number
 
-    constructor(name: String, combos: { [key: string]: string } = {}, sprite: Image) {
+    constructor(name: String, combos: { [key: string]: string } = {}, sprite: Image, scale: number) {
         this.name = name
         this.combos = combos
         this.sprite = sprite
@@ -12,6 +13,7 @@ class Element {
     toSprite(): Sprite {
         let sprite = sprites.create(this.sprite, SpriteKind.Element)
         sprite.data = this
+        sprite.setScale(1/this.scale, ScaleAnchor.Middle)
         return sprite
     }
 }
